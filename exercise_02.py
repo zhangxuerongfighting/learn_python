15a513cbea33619da7f8d570fe7de01d63d9e278
zhangxuerongfighting <113277787+zhangxuerongfighting@users.noreply.github.com>
2022-09-14 21:59:18 +0800

    Update exercise_02.py
    我的第二次作业

--- exercise_02.py
+++ exercise_02.py
@@ -1,3 +1,7 @@
+Exercise 2: Write another program that prompts for a list of numbers
+as above and at the end prints out both the maximum and minimum of
+the numbers instead of the average
+
 total = 0
 average = 0
 
