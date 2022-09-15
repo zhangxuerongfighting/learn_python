86eaefa17d5a0f044bfaf74e7c85020adfb7e932
zhangxuerongfighting <113277787+zhangxuerongfighting@users.noreply.github.com>
2022-09-15 17:31:14 +0800

    Update exercise_02.py
    姓名：张雪蓉，学号：2020144137，我的第二次作业

--- exercise_02.py
+++ exercise_02.py
@@ -1,6 +1,8 @@
-Exercise 2: Write another program that prompts for a list of numbers
-as above and at the end prints out both the maximum and minimum of
-the numbers instead of the average
+Exercise 1: Write a program which repeatedly reads numbers until the
+user enters “done”. Once “done” is entered, print out the total, count,
+and average of the numbers. If the user enters anything other than a
+number, detect their mistake using try and except and print an error
+message and skip to the next number.
 
 total = 0
 average = 0
