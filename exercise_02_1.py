24ea7b10d40e452e288a313d23a4a03bfeaf5faf
zhangxuerongfighting <113277787+zhangxuerongfighting@users.noreply.github.com>
2022-09-15 17:18:53 +0800

    Update exercise_02_1.py
    学号：2020144137，姓名：张雪蓉，我的第二次作业

--- exercise_02_1.py
+++ exercise_02_1.py
@@ -1,8 +1,5 @@
-Exercise 5: Take the following Python code that stores a string:
-str = 'X-DSPAM-Confidence:0.8475'
-Use find and string slicing to extract the portion of the string after the
-colon character and then use the float function to convert the extracted
-string into a floating point number.
+Exercise 5: Take the following Python code that stores a string:str = 'X-DSPAM-Confidence:0.8475'
+Use find and string slicing to extract the portion of the string after the colon character and then use the float function to convert the extracted string into a floating point number.
 
 txt = "X-DSPAM-Confidence:0.8475"
 count = txt.find('0')         
